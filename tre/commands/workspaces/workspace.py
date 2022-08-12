67d375a7a232c2fd0f07e37de608696871b2025a
Stuart Leeks <stuartle@microsoft.com>
2022-08-12 19:39:07 +0000

    Add workspace id tab completion

--- tre/commands/workspaces/workspace.py
+++ tre/commands/workspaces/workspace.py
@@ -14,17 +14,26 @@ from .airlock.requests import airlocks
 from .airlock.request import airlock
 
 
-@click.group(invoke_without_command=True, help="Perform actions on an individual workspace")
-@click.argument('workspace_id', envvar='TRECLI_WORKSPACE_ID', required=True)
-@click.pass_context
+def workspace_id_completion(ctx, param, incomplete):
+    log = logging.getLogger(__name__)
+    client = ApiClient.get_api_client_from_config()
+    response = client.call_api(log, 'GET', '/api/workspaces')
+    if response.is_success:
+        ids = [workspace["id"] for workspace in response.json()["workspaces"]]
+        return [id for id in ids if id.startswith(incomplete)]
+
+
+@ click.group(invoke_without_command=True, help="Perform actions on an individual workspace")
+@ click.argument('workspace_id', envvar='TRECLI_WORKSPACE_ID', required=True, shell_complete=workspace_id_completion)
+@ click.pass_context
 def workspace(ctx: click.Context, workspace_id: str) -> None:
     ctx.obj = WorkspaceContext(workspace_id)
 
 
-@click.command(name="show", help="Show a workspace")
-@click.option('--output', '-o', 'output_format', default='json', type=click.Choice(['table', 'json', 'none']), help="Output format")
-@click.option('--query', '-q', default=None, help="JMESPath query to apply to the result")
-@pass_workspace_context
+@ click.command(name="show", help="Show a workspace")
+@ click.option('--output', '-o', 'output_format', default='json', type=click.Choice(['table', 'json', 'none']), help="Output format")
+@ click.option('--query', '-q', default=None, help="JMESPath query to apply to the result")
+@ pass_workspace_context
 def workspace_show(workspace_context: WorkspaceContext, output_format, query):
     log = logging.getLogger(__name__)
 
@@ -43,23 +52,23 @@ def workspace_show(workspace_context: WorkspaceContext, output_format, query):
     return response.text
 
 
-@click.command(name="set-enabled", help="Enable/disable a workspace")
-@click.option('--etag',
-              help='The etag of the workspace to update',
-              required=True)
-@click.option('--enable/--disable', is_flag=True, required=True)
-@click.option('--wait-for-completion',
-              flag_value=True,
-              default=False)
-@click.option('--output', '-o', 'output_format',
-              help="Output format",
-              type=click.Choice(['table', 'json', 'none']),
-              default='json')
-@click.option('--query', '-q',
-              help="JMESPath query to apply to the result",
-              default=None)
-@click.pass_context
-@pass_workspace_context
+@ click.command(name="set-enabled", help="Enable/disable a workspace")
+@ click.option('--etag',
+               help='The etag of the workspace to update',
+               required=True)
+@ click.option('--enable/--disable', is_flag=True, required=True)
+@ click.option('--wait-for-completion',
+               flag_value=True,
+               default=False)
+@ click.option('--output', '-o', 'output_format',
+               help="Output format",
+               type=click.Choice(['table', 'json', 'none']),
+               default='json')
+@ click.option('--query', '-q',
+               help="JMESPath query to apply to the result",
+               default=None)
+@ click.pass_context
+@ pass_workspace_context
 def workspace_set_enabled(workspace_context: WorkspaceContext, ctx: click.Context, etag, enable, wait_for_completion, output_format, query, suppress_output: bool = False):
     log = logging.getLogger(__name__)
 
@@ -83,23 +92,23 @@ def workspace_set_enabled(workspace_context: WorkspaceContext, ctx: click.Contex
             output(response.text, output_format=output_format, query=query, default_table_query=default_operation_table_query())
 
 
-@click.command(name="delete", help="Delete a workspace")
-@click.option('--yes', is_flag=True, default=False)
-@click.option('--wait-for-completion',
-              flag_value=True,
-              help="Wait for the operation to complete if it is in progress",
-              default=False)
-@click.option('--ensure-disabled',
-              help="Disable before deleting if not currently enabled",
-              flag_value=True,
-              default=False)
-@click.option('--output', '-o', 'output_format',
-              help="Output format",
-              type=click.Choice(['table', 'json', 'none']),
-              default='json')
-@click.option('--query', '-q',
-              help="JMESPath query to apply to the result",
-              default=None)
+@ click.command(name="delete", help="Delete a workspace")
+@ click.option('--yes', is_flag=True, default=False)
+@ click.option('--wait-for-completion',
+               flag_value=True,
+               help="Wait for the operation to complete if it is in progress",
+               default=False)
+@ click.option('--ensure-disabled',
+               help="Disable before deleting if not currently enabled",
+               flag_value=True,
+               default=False)
+@ click.option('--output', '-o', 'output_format',
+               help="Output format",
+               type=click.Choice(['table', 'json', 'none']),
+               default='json')
+@ click.option('--query', '-q',
+               help="JMESPath query to apply to the result",
+               default=None)
 @ click.pass_context
 @ pass_workspace_context
 def workspace_delete(workspace_context: WorkspaceContext, ctx: click.Context, yes, wait_for_completion, ensure_disabled, output_format, query):
